29ffe322e89aa8b1d113b6e97305ad8311c8b880
Luis Mayta <luis.mayta@gmail.com>
2022-01-10 17:19:30 -0500

    Usando rutas con un model

---
+++ lib/models/models.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_familia/models/menu_models.dart';
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_familia/models/models.dart';
 import 'package:flutter_familia/screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
 
+  static final menuOptions = <MenuModels>[
+
+    MenuModels('detalle_miembro','Detalle de miembro',const DetalleMiembroScreen()),
+  ];
+
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
     'detalle_miembro': (BuildContext context) => const DetalleMiembroScreen()
   };
-  static Route<dynamic> onGenerateRoute (RouteSettings settings) {
-        return MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
-        );
-      }
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(
+      builder: (context) => const HomeScreen(),
+    );
+  }
 }
